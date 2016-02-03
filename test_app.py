59dbcbfe90fd91d54dd4e93a96212bd2a7e3cb3a
Pete Wildsmith <pete@dabapps.com>
2016-02-03 16:09:49 +0000

    actiually add tests

---
+++ test_app.py
@@ -0,0 +1,15 @@
+from coreapi.document import Document
+from django.conf.urls import url
+from django_coreapi.renderers import CoreAPIJSONRenderer
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.response import Response
+
+
+@api_view(['GET'])
+@renderer_classes([CoreAPIJSONRenderer])
+def document(request):
+    return Response(Document(title='Test Document', url='/'))
+
+urlpatterns = [
+    url('', document),
+]
