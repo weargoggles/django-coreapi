144e2f18c2b754b962c6d7efb5f4e5fa0aefa92f
Pete Wildsmith <pete@dabapps.com>
2016-02-24 11:37:26 +0000

    mocking support

---
+++ django_coreapi/mock.py
@@ -0,0 +1,66 @@
+import functools
+import coreapi
+import django_coreapi.client
+
+
+_responses = []
+
+
+def get_match(keys):
+    """
+    Find the first matching response in the current set
+    :param keys: the key path to be matched
+    :return: the matching response or None
+    """
+    for response in _responses:
+        if response[0] == keys:
+            return response[1]
+
+
+class Mock(object):
+    """
+    Can be used as a context manager. Takes handler functions as arguments, which are evaluated in order in place of
+    """
+    def __init__(self):
+        super(Mock, self).__init__()
+
+    def __enter__(self):
+        # prepare and inject the mock methods to coreapi's session
+        self._real_action = coreapi.Client.action
+        self._real_django_client_action = django_coreapi.client.DjangoCoreAPIClient.action
+
+        def fake_action(client, document, keys, *args, **kwargs):
+            res = get_match(keys)
+            if res is not None:
+                return res
+            raise
+
+        coreapi.Client.action = fake_action
+        django_coreapi.client.DjangoCoreAPIClient.action = fake_action
+
+    def __exit__(self, exc_type, value, tb):
+        global _responses
+        # replace the real methods
+        coreapi.Client.action = self._real_action
+        django_coreapi.client.DjangoCoreAPIClient.action = self._real_django_client_action
+        # clear out the match list
+        _responses = []
+
+
+def activate(f):
+    """
+    A decorator which mocks the coreapi and django_coreapi clients, allowing use of `add`
+    :param f: the function to be wrapped
+    :return: the wrapped function
+    """
+    @functools.wraps(f)
+    def decorated(*args, **kwargs):
+        with Mock():
+            return f(*args, **kwargs)
+
+    return decorated
+
+
+def add(keys, response):
+    global _responses
+    _responses.append((keys, response))
--- tests.py
+++ tests.py
@@ -4,6 +4,7 @@ from django.conf import settings
 settings.configure(ROOT_URLCONF='test_app', DEBUG=True, REST_FRAMEWORK={'UNAUTHENTICATED_USER': None})
 import django
 django.setup()
+from django_coreapi import mock
 from django_coreapi.client import DjangoCoreAPIClient
 from django_coreapi.transports import DjangoTestHTTPTransport
 
@@ -35,3 +36,20 @@ class Tests(unittest.TestCase):
             'test': 'cat'
         }})
         self.assertIsNotNone(doc)
+
+    @mock.activate
+    def test_mocking(self):
+        mock.add(['test', 'post_data'], {"a": 1})
+        content = {
+            'test': {
+                'post_data': Link(url='/post_data/', action='post', fields=[
+                    Field('data', location='body')
+                ]),
+            }
+        }
+        schema = Document(title='test', content=content)
+        client = DjangoCoreAPIClient()
+        doc = client.action(schema, ['test', 'post_data'], params={'data': {
+            'test': 'cat'
+        }})
+        self.assertEqual(doc, {"a": 1})
